7be811125bff0ed764be2125ec836d7fb7bc9151
ParisiGP <gustavoparisi12@gmail.com>
2023-11-09 19:08:45 -0300

    Novo Portifolio

--- js/script.js
+++ js/script.js
@@ -8,3 +8,19 @@ function lightMode(element) {
         document.body.classList.add('light')
     }
 }
+
+function playVideo(ID_) {
+    document.querySelector('.ReactImage'+ID_).style.display = 'none';
+    document.querySelector('.ReactVideo'+ID_).style.display = 'block';
+    document.querySelector('.ReactVideo'+ID_).play();
+
+  }
+
+  function pauseVideo(ID_) {
+    document.querySelector('.ReactVideo'+ID_).pause();
+    document.querySelector('.ReactVideo'+ID_).currentTime = 0;
+    document.querySelector('.ReactVideo'+ID_).style.display = 'none';
+    document.querySelector('.ReactImage'+ID_).style.display = 'block';
+  }
+
+
