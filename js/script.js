548c9c41fff9e535e32abca1e825e083544fb0f3
ParisiGP <gustavoparisi12@gmail.com>
2023-11-10 16:28:23 -0300

    Tentativa fullscreen

--- js/script.js
+++ js/script.js
@@ -9,18 +9,63 @@ function lightMode(element) {
     }
 }
 
+
 function playVideo(ID_) {
-    document.querySelector('.ReactImage'+ID_).style.display = 'none';
-    document.querySelector('.ReactVideo'+ID_).style.display = 'block';
-    document.querySelector('.ReactVideo'+ID_).play();
+    document.querySelector('.ReactVideo' + ID_).play();
+    document.querySelector('.ReactVideo' + ID_).controls = true;
 
-  }
+}
 
-  function pauseVideo(ID_) {
-    document.querySelector('.ReactVideo'+ID_).pause();
-    document.querySelector('.ReactVideo'+ID_).currentTime = 0;
-    document.querySelector('.ReactVideo'+ID_).style.display = 'none';
-    document.querySelector('.ReactImage'+ID_).style.display = 'block';
-  }
+function pauseVideo(ID_) {
+    document.querySelector('.ReactVideo' + ID_).pause();
+    document.querySelector('.ReactVideo' + ID_).currentTime = 0;
+    document.querySelector('.ReactVideo' + ID_).controls = false;
+}
+
+var meuVideo = document.getElementById("meuVideo");
+
+// Adiciona um listener para detectar alterações no estado fullscreen
+
+
+// Adiciona um listener para detectar a tecla ESC para sair do fullscreen
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" && document.fullscreenElement) {
+        exitFullscreen();
+    }
+});
+
+function toggleFullscreen() {
+    if (!document.fullscreenElement) {
+        enterFullscreen(meuVideo);
+    } else {
+        exitFullscreen();
+    }
+}
+
+function enterFullscreen(element) {
+    document.querySelector('.ReactVideo1').style.display = 'block';
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen();
+    } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen();
+    }
+}
+
+function exitFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+    }
+}
+
 
 
