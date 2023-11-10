8cde736418cf9b8f30c537fdf4d6867bb4b6ccbd
ParisiGP <gustavoparisi12@gmail.com>
2023-11-10 18:37:22 -0300

    Novo desining

--- js/script.js
+++ js/script.js
@@ -9,134 +9,36 @@ function lightMode(element) {
     }
 }
 
+function RotateCard(Card_) {
 
-function playVideo(ID_) {
-    document.querySelector('.ReactVideo' + ID_).play();
-    document.querySelector('.ReactVideo' + ID_).controls = true;
+    let front = document.getElementsByClassName("frontCard");
+    let back = document.getElementsByClassName("backCard");
 
-}function lightMode(element) {
-    console.log(document.body.classList)
+    for (let i = 0; i < front.length; i++) {
+        if (Card_.style.transform == "rotateY(180deg)") {
+            Card_.style.transform = "rotateY(0deg)";
 
-    if (element.checked) {
-        document.body.classList.remove('light')
-    } else {
+            setTimeout(() => {
+                back[i].style.display = "none";
+                front[i].style.display = "block";
+            }, 350);
+            return;
+        } else {
+            Card_.style.transform = "rotateY(180deg)";
 
-        document.body.classList.add('light')
+            setTimeout(() => {
+                back[i].style.display = "flex";
+                front[i].style.display = "none";
+            }, 350);
+            return;
+        }
     }
+
+
+
+
+
+
 }
-
-
-function playVideo(ID_) {
-    document.querySelector('.ReactVideo' + ID_).play();
-    document.querySelector('.ReactVideo' + ID_).controls = true;
-
-}
-
-function pauseVideo(ID_) {
-    document.querySelector('.ReactVideo' + ID_).pause();
-    document.querySelector('.ReactVideo' + ID_).currentTime = 0;
-    document.querySelector('.ReactVideo' + ID_).controls = false;
-}
-
-var meuVideo = document.getElementById("meuVideo");
-
-// Adiciona um listener para detectar alterações no estado fullscreen
-
-
-// Adiciona um listener para detectar a tecla ESC para sair do fullscreen
-document.addEventListener("keydown", function (e) {
-    if (e.key === "Escape" && document.fullscreenElement) {
-        exitFullscreen();
-    }
-});
-
-function toggleFullscreen() {
-    if (!document.fullscreenElement) {
-        enterFullscreen(meuVideo);
-    } else {
-        exitFullscreen();
-    }
-}
-
-function enterFullscreen(element) {
-    document.querySelector('.ReactVideo1').style.display = 'block';
-    if (element.requestFullscreen) {
-        element.requestFullscreen();
-    } else if (element.mozRequestFullScreen) {
-        element.mozRequestFullScreen();
-    } else if (element.webkitRequestFullscreen) {
-        element.webkitRequestFullscreen();
-    } else if (element.msRequestFullscreen) {
-        element.msRequestFullscreen();
-    }
-}
-
-function exitFullscreen() {
-    if (document.exitFullscreen) {
-        document.exitFullscreen();
-    } else if (document.mozCancelFullScreen) {
-        document.mozCancelFullScreen();
-    } else if (document.webkitExitFullscreen) {
-        document.webkitExitFullscreen();
-    } else if (document.msExitFullscreen) {
-        document.msExitFullscreen();
-    }
-}
-
-
-
-
-
-function pauseVideo(ID_) {
-    document.querySelector('.ReactVideo' + ID_).pause();
-    document.querySelector('.ReactVideo' + ID_).currentTime = 0;
-    document.querySelector('.ReactVideo' + ID_).controls = false;
-}
-
-var meuVideo = document.getElementById("meuVideo");
-
-// Adiciona um listener para detectar alterações no estado fullscreen
-
-
-// Adiciona um listener para detectar a tecla ESC para sair do fullscreen
-document.addEventListener("keydown", function (e) {
-    if (e.key === "Escape" && document.fullscreenElement) {
-        exitFullscreen();
-    }
-});
-
-function toggleFullscreen() {
-    if (!document.fullscreenElement) {
-        enterFullscreen(meuVideo);
-    } else {
-        exitFullscreen();
-    }
-}
-
-function enterFullscreen(element) {
-    document.querySelector('.ReactVideo1').style.display = 'block';
-    if (element.requestFullscreen) {
-        element.requestFullscreen();
-    } else if (element.mozRequestFullScreen) {
-        element.mozRequestFullScreen();
-    } else if (element.webkitRequestFullscreen) {
-        element.webkitRequestFullscreen();
-    } else if (element.msRequestFullscreen) {
-        element.msRequestFullscreen();
-    }
-}
-
-function exitFullscreen() {
-    if (document.exitFullscreen) {
-        document.exitFullscreen();
-    } else if (document.mozCancelFullScreen) {
-        document.mozCancelFullScreen();
-    } else if (document.webkitExitFullscreen) {
-        document.webkitExitFullscreen();
-    } else if (document.msExitFullscreen) {
-        document.msExitFullscreen();
-    }
-}
-
 
 
